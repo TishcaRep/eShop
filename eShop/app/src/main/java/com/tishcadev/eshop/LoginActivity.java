52485bafb29566081a01a06e3afc0499314e837e
Unknown <julio.romotishca@gmail.com>
2019-03-07 20:35:08 -0600

    Update

---
+++ eShop/app/src/main/java/com/tishcadev/eshop/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.tishcadev.eshop;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
